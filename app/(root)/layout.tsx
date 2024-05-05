d8d6780183c88493911429c0209edb322f5b42c3
BuildTools <unconfigured@null.spigotmc.org>
2024-05-05 15:36:58 -0700

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,24 @@
+import SideBar from "@/components/SideBar";
+import MobileNav from "@/components/MobileNav";
 import React from "react";
+import Image from "next/image";
 
 const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  const login = { firstName: "Tony", lastName: "Crimson" };
+
   return (
-    <main>
-      sidebar
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={login} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.png" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={login} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 };
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,41 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const login = {
+    firstName: "Tony",
+    lastName: "Crimson",
+    email: "Tpeeranat@micamp.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={login?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSideBar
+        user={login}
+        transactions={[]}
+        banks={[{ currentBalance: 500 }, { currentBalance: 230.55 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
